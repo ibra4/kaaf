8bceb97857731edb9ea3c1b6b5bb97d76706ae66
ibra <i.hammad@syarah.com>
2022-05-14 18:57:12 +0300

    Fixes

--- index.php
+++ index.php
@@ -49,7 +49,9 @@
             <div class="collapse navbar-collapse p-4" id="navbarSearchContent">
                 <input type="text" class="form-control search" name="search" placeholder="Search Projects">
             </div>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+            <!-- Start Desktop Collapse -->
+            <div class="collapse navbar-collapse desktop-menu" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
 
                     <li class="nav-item dropdown">
@@ -229,19 +231,19 @@
                         <div class="dropdown-menu flags-menu" aria-labelledby="navbarDropdown">
                             <a class="dropdown-item active" href="#">
                                 BHD
-                                <img src="./icon/flags/bh.svg" alt="">
+                                <img class="flag" src="./icon/flags/bh.svg" alt="">
                             </a>
                             <a class="dropdown-item" href="#">
                                 KWT
-                                <img src="./icon/flags/kw.svg" alt="">
+                                <img class="flag" src="./icon/flags/kw.svg" alt="">
                             </a>
                             <a class="dropdown-item" href="#">
                                 AED
-                                <img src="./icon/flags/ae.svg" alt="">
+                                <img class="flag" src="./icon/flags/ae.svg" alt="">
                             </a>
                             <a class="dropdown-item" href="#">
                                 SAR
-                                <img src="./icon/flags/sa.svg" alt="">
+                                <img class="flag" src="./icon/flags/sa.svg" alt="">
                             </a>
                         </div>
                     </li>
@@ -266,6 +268,204 @@
                     </li>
                 </ul>
             </div>
+            <!-- End Desktop Collapse -->
+
+
+
+            <!-- Start Mobile Collapse -->
+            <div class="collapse navbar-collapse mobile-menu" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="fa fa-user"></i>
+                            <span class="px-2">Sign In</span>
+                        </a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <img class="flag" src="./icon/flags/bh.svg" alt="">
+                            <span class="px-2">Curruncy (BHD)</span>
+                        </a>
+                        <div class="dropdown-menu flags-menu" aria-labelledby="navbarDropdown">
+                            <div class="back-to-main-menu">Back to main menu</div>
+                            <div class="dropdown-title">Curruncy</div>
+                            <a class="dropdown-item active" href="#">
+                                <img class="flag" src="./icon/flags/bh.svg" alt="">
+                                BHD
+                            </a>
+                            <a class="dropdown-item" href="#">
+                                <img class="flag" src="./icon/flags/kw.svg" alt="">
+                                KWT
+                            </a>
+                            <a class="dropdown-item" href="#">
+                                <img class="flag" src="./icon/flags/ae.svg" alt="">
+                                AED
+                            </a>
+                            <a class="dropdown-item" href="#">
+                                <img class="flag" src="./icon/flags/sa.svg" alt="">
+                                SAR
+                            </a>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarKafaratDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Sadaqat & Kafarat
+                        </a>
+                        <div class="dropdown-menu">
+                            <div class="back-to-main-menu">Back to main menu</div>
+                            <div class="dropdown-title">Sadaqat & Kafarat</div>
+                            <div class="dropdown-item has-submenu">
+                                <span>Ongoing sadaqa</span>
+                                <div class="dropdown-menu">
+                                    <div class="back-to-main-menu">Back to Ongoing sadaqa</div>
+                                    <div class="dropdown-title"></div>
+                                    <a class="dropdown-item" href="#">Ongoing sadaqa 1</a>
+                                    <a class="dropdown-item" href="#">Ongoing sadaqa 2</a>
+                                    <a class="dropdown-item" href="#">Ongoing sadaqa 3</a>
+                                    <a class="dropdown-item color-yellow" href="#">Ongoing sadaqa 4</a>
+                                </div>
+                            </div>
+                            <div class="dropdown-item has-submenu">
+                                <span>Sadaqa</span>
+                                <div class="dropdown-menu">
+                                    <div class="back-to-main-menu">Back to Sadaqa</div>
+                                    <div class="dropdown-title"></div>
+                                    <a class="dropdown-item" href="#">Sadaqa 1</a>
+                                    <a class="dropdown-item" href="#">Sadaqa 2</a>
+                                    <a class="dropdown-item" href="#">Sadaqa 3</a>
+                                    <a class="dropdown-item" href="#">Sadaqa 4</a>
+                                </div>
+                            </div>
+                            <div class="dropdown-item has-submenu">
+                                <span>Kafara</span>
+                                <div class="dropdown-menu">
+                                    <div class="back-to-main-menu">Back to Kafara</div>
+                                    <div class="dropdown-title"></div>
+                                    <a class="dropdown-item" href="#">Kafara 1</a>
+                                    <a class="dropdown-item" href="#">Kafara 2</a>
+                                    <a class="dropdown-item" href="#">Kafara 3</a>
+                                    <a class="dropdown-item" href="#">Kafara 4</a>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li class="nav-item"><a href="#" class="nav-link">Projects</a></li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarProjectsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Bahrain Projects
+                        </a>
+                        <div class="dropdown-menu">
+                            <div class="back-to-main-menu">Back to main menu</div>
+                            <div class="dropdown-title">Bahrain Projects</div>
+                            <div class="dropdown-item has-submenu">
+                                <span>Social Projects</span>
+                                <div class="dropdown-menu">
+                                    <div class="back-to-main-menu">Back to Social Projects</div>
+                                    <div class="dropdown-title"></div>
+                                    <a class="dropdown-item" href="#">Social Projects 1</a>
+                                    <a class="dropdown-item" href="#">Social Projects 2</a>
+                                    <a class="dropdown-item" href="#">Social Projects 3</a>
+                                    <a class="dropdown-item color-yellow" href="#">Social Projects 4</a>
+                                </div>
+                            </div>
+                            <div class="dropdown-item has-submenu">
+                                <span>Housing and Restoration Projects</span>
+                                <div class="dropdown-menu">
+                                    <div class="back-to-main-menu">Back to Housing and Restoration Projects</div>
+                                    <div class="dropdown-title"></div>
+                                    <a class="dropdown-item" href="#">Housing 1</a>
+                                    <a class="dropdown-item" href="#">Housing 2</a>
+                                    <a class="dropdown-item" href="#">Housing 3</a>
+                                    <a class="dropdown-item color-yellow" href="#">Housing 4</a>
+                                </div>
+                            </div>
+                            <div class="dropdown-item has-submenu">
+                                <span>Educational Projects</span>
+                                <div class="dropdown-menu">
+                                    <div class="back-to-main-menu">Back to Educational Projects</div>
+                                    <div class="dropdown-title"></div>
+                                    <a class="dropdown-item" href="#">Educational Projects 1</a>
+                                    <a class="dropdown-item" href="#">Educational Projects 2</a>
+                                    <a class="dropdown-item" href="#">Educational Projects 3</a>
+                                    <a class="dropdown-item" href="#">Educational Projects 4</a>
+                                </div>
+                            </div>
+
+                            <div class="dropdown-item has-submenu">
+                                <span>Eslah Community Services Center</span>
+                                <div class="dropdown-menu">
+                                    <div class="back-to-main-menu">Back to Eslah Community Services Center</div>
+                                    <div class="dropdown-title"></div>
+                                    <a class="dropdown-item" href="#">Eslah Community 1</a>
+                                    <a class="dropdown-item" href="#">Eslah Community 2</a>
+                                    <a class="dropdown-item" href="#">Eslah Community 3</a>
+                                    <a class="dropdown-item" href="#">Eslah Community 4</a>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarReliefDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Relief
+                        </a>
+                        <div class="dropdown-menu">
+                            <div class="back-to-main-menu">Back to main menu</div>
+                            <div class="dropdown-title">Relief</div>
+                            <div class="dropdown-item has-submenu">
+                                <span>Countries</span>
+                                <div class="dropdown-menu">
+                                    <div class="back-to-main-menu">Back to Relief</div>
+                                    <div class="dropdown-title"></div>
+                                    <a class="dropdown-item" href="#">Countries 1</a>
+                                    <a class="dropdown-item" href="#">Countries 2</a>
+                                    <a class="dropdown-item" href="#">Countries 3</a>
+                                    <a class="dropdown-item color-yellow" href="#">Countries 4</a>
+                                </div>
+                            </div>
+                            <div class="dropdown-item has-submenu">
+                                <span>Journies</span>
+                                <div class="dropdown-menu">
+                                    <div class="back-to-main-menu">Back to Journies</div>
+                                    <div class="dropdown-title"></div>
+                                    <a class="dropdown-item" href="#">Journies 1</a>
+                                    <a class="dropdown-item" href="#">Journies 2</a>
+                                    <a class="dropdown-item" href="#">Journies 3</a>
+                                    <a class="dropdown-item" href="#">Journies 4</a>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="nav-item"><a href="#" class="nav-link">Zakat</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Kafalat</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Kaaf?</a></li>
+                    <li class="nav-item d-flex align-items-center seasonal-container mx-2"><button class="btn btn-yellow btn-border-radius">Seasonal Projects</button></li>
+
+                    <li class="nav-item d-none d-lg-block">
+                        <form action="hahahaha" method="post" class="icon-input">
+                            <input type="text" placeholder="General search in the website" id="">
+                            <input type="image" src="./icon/search.svg" alt="Submit" />
+                        </form>
+                    </li>
+                    <li class="nav-item d-none d-lg-block">
+                        <a href="#" class="lang-button">
+                            ع
+                        </a>
+                    </li>
+                    <li class="nav-item donation-block d-none d-lg-block px-3">
+                        <a href="#" class="nav-link">
+                            <i class="fa fa-shopping-cart ml-2"></i>
+                            <span class="px-2s">Donation Cart</span>
+                            <span class="notifications-count">3</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <!-- End Mobile Collapse -->
+
+
+
         </nav>
     </div>
     <section id="slider">
