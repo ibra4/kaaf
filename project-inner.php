220a7cbd7742bd229740cb8f10a10ca92aa48ec4
ibra <i.hammad@syarah.com>
2022-05-16 21:07:13 +0300

    Page inner

---
+++ project-inner.php
@@ -0,0 +1,37 @@
+<?php require_once "./hmtl-components/en/head.php" ?>
+<?php require_once "./hmtl-components/en/navbar.php" ?>
+
+<main class="py-5">
+    <div class="container">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb px-0">
+                <li class="breadcrumb-item">Home</li>
+                <li class="breadcrumb-item">Library</li>
+                <li class="breadcrumb-item">Data</li>
+            </ol>
+        </nav>
+        <div class="p-2 bg-white rounded">
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="/assets/images/waterwell.jpeg" class="w-100">
+                </div>
+                <div class="col-md-8 py-3 px-4">
+                    <span class="status-label urgant">urgant</span>
+                    <h2 class="mt-4">Semi-artesian well 60-70m</h2>
+                    <div class="d-flex justify-content-start align-items-center">
+                        <div class="bg-light-blue p-1">
+                            <i class="fa fa-star"></i>
+                        </div>
+                        <span class="pl-2">Somallia</span>
+                        <div>
+                            <i class="fa fa-upload"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+<?php require_once "./hmtl-components/en/footer.php" ?>
+<?php require_once "./hmtl-components/en/scripts.php" ?>
