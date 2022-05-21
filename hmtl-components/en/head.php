54bf2600aa8d3e0ba9e01abe5d1734c93eca79e4
ibra <i.hammad@syarah.com>
2022-05-22 02:17:57 +0300

    CSS Version

--- hmtl-components/en/head.php
+++ hmtl-components/en/head.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/assets/css/style.css?v=12">
+    <link rel="stylesheet" href="/assets/css/style.css?v=13">
 
     <link rel="stylesheet" type="text/css" href="/assets/css/slick.css" />
     <link rel="stylesheet" type="text/css" href="/assets/css/slick-theme.css" />
