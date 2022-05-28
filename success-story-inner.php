475a27fe388463770272c4556cf6cfa358022d2e
ibra4 <ibra16@itc.hu.edu.jo>
2022-05-28 17:34:47 +0300

    Success Stories

---
+++ success-story-inner.php
@@ -0,0 +1,60 @@
+<?php require_once "./hmtl-components/en/head.php" ?>
+<?php require_once "./hmtl-components/en/navbar.php" ?>
+
+<div class="slide-image banner d-flex align-items-center" style="background-image: url('/assets/images/man-child.png');">
+    <div class="hero-inner m-auto">
+        <h1 class="color-white banner-title">Lorem ipsum dolor sit amet</h1>
+        <p class="color-white banner-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+</div>
+<main class="py-5 container">
+    <div class="row">
+        <div class="col-lg-6 mb-3">
+            <div class="success-stories-item bg-white p-3">
+                <img class="rounded" src="/assets/images/arms.png" alt="">
+                <div class="container">
+                    <h2 class="font-size-lg my-3">Lorem ipsum dolor sit amet</h2>
+                    <p class="color-dark-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                    <a href="#" class="btn btn-lg btn-purple rounded">Read more</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6 mb-3">
+            <div class="success-stories-item bg-white p-3">
+                <img class="rounded" src="/assets/images/arms.png" alt="">
+                <div class="container">
+                    <h2 class="font-size-lg my-3">Lorem ipsum dolor sit amet</h2>
+                    <p class="color-dark-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                    <a href="#" class="btn btn-lg btn-purple rounded">Read more</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6 mb-3">
+            <div class="success-stories-item bg-white p-3">
+                <img class="rounded" src="/assets/images/arms.png" alt="">
+                <div class="container">
+                    <h2 class="font-size-lg my-3">Lorem ipsum dolor sit amet</h2>
+                    <p class="color-dark-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                    <a href="#" class="btn btn-lg btn-purple rounded">Read more</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6 mb-3">
+            <div class="success-stories-item bg-white p-3">
+                <img class="rounded" src="/assets/images/arms.png" alt="">
+                <div class="container">
+                    <h2 class="font-size-lg my-3">Lorem ipsum dolor sit amet</h2>
+                    <p class="color-dark-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                    <a href="#" class="btn btn-lg btn-purple rounded">Read more</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="text-center">
+        <button class="btn btn-lg btn-purple rounded btn-wide mt-5">Load more</button>
+    </div>
+</main>
+
+<?php require_once "./hmtl-components/en/footer.php" ?>
+<?php require_once "./hmtl-components/en/login.php" ?>
+<?php require_once "./hmtl-components/en/scripts.php" ?>
