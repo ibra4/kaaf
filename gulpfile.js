ca9469113f5c6fe848c10ac853db7e590a740f1a
ibra4 <ibra16@itc.hu.edu.jo>
2022-05-28 17:34:46 +0300

    Page inner

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,15 @@ var gulp = require("gulp");
 var rtlcss = require("gulp-rtlcss");
 const sass = require("gulp-sass")(require("sass"));
 var rename = require("gulp-rename");
+const livereload = require("gulp-livereload");
 
 gulp.task("build-sass", () => {
   return gulp
     .src("assets/sass/main.scss")
     .pipe(rename("style.css"))
     .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
-    .pipe(gulp.dest("assets/css"));
+    .pipe(gulp.dest("assets/css"))
+    .pipe(livereload());
 });
 
 gulp.task("rtl", function () {
@@ -20,6 +22,7 @@ gulp.task("rtl", function () {
 });
 
 gulp.task("watch", () => {
+  livereload.listen();
   gulp.watch("assets/sass/*.scss", (done) => {
     gulp.series(["build-sass", "rtl"])(done);
   });
