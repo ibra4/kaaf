6e30a595705f34c33478545b7b85e0043a870276
ibra4 <ibra16@itc.hu.edu.jo>
2022-05-28 17:34:46 +0300

    Responsive slick

--- js/main.js
+++ js/main.js
@@ -5,8 +5,10 @@ $(function () {
   slider.slick({
     dots: true,
     appendArrows: ".slick-custom-buttons",
-    nextArrow: "<div class='slick-custom-next'></div>",
-    prevArrow: "<div class='slick-custom-prev'></div>",
+    nextArrow:
+      "<div class='slick-custom-next'><i class='fa fa-chevron-right'></i></div>",
+    prevArrow:
+      "<div class='slick-custom-prev'><i class='fa fa-chevron-left'></i></div>",
   });
 
   $(".slick-custom-buttons .slick-custom-prev").on("click", function () {
@@ -22,31 +24,52 @@ $(function () {
     slidesToShow: 3.5,
     slidesToScroll: 1,
     appendArrows: ".latest-projects-slick-buttons",
-    nextArrow: "<div class='latest-projects-slick-custom-next'></div>",
-    prevArrow: "<div class='latest-projects-slick-custom-prev'></div>",
+    nextArrow:
+      "<div class='latest-projects-slick-custom-next'><i class='fa fa-chevron-right'></i></div>",
+    prevArrow:
+      "<div class='latest-projects-slick-custom-prev'><i class='fa fa-chevron-left'></i></div>",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
 
-  $('#decrease-shares').on('click', function (e) {
-    e.preventDefault()
-    console.log($(this).parent().find("input"))
-    const sharesElement = $('#shares-value')
-    let value = parseInt(sharesElement.val())
-    if (value > 5) {
-      sharesElement.val(value - 5)
-    }
-  })
-  $('#increase-shares').on('click', function (e) {
-    e.preventDefault()
-    const sharesElement = $('#shares-value')
-    let value = parseInt(sharesElement.val())
-    sharesElement.val(value + 5)
-  })
+  $(".increase-5").on("click", function (e) {
+    e.preventDefault();
+    changeCount(this, "increase");
+  });
 
-  $('#increase-peojects').on('click', function (e) {
-    e.preventDefault()
-    const sharesElement = $('#shares-value')
-    let value = parseInt(sharesElement.val())
-    sharesElement.val(value + 5)
-  })
-  
+  $(".decrease-5").on("click", function (e) {
+    e.preventDefault();
+    changeCount(this, "decrease");
+  });
+
+  function changeCount(button, type) {
+    const targetInput = $(button).parent().find("input");
+    let value = parseInt(targetInput.val());
+    if (type == "increase") {
+      targetInput.val(value + 5);
+    } else if (type == "decrease" && value > 5) {
+      targetInput.val(value - 5);
+    }
+  }
 });
