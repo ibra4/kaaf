f92b99c0172997150454e2c06db4b2b7360b9f71
ibra4 <ibra16@itc.hu.edu.jo>
2022-05-31 03:47:02 +0300

    Continue

--- success-story-inner.php
+++ success-story-inner.php
@@ -1,60 +1,64 @@
 <?php require_once "./hmtl-components/en/head.php" ?>
 <?php require_once "./hmtl-components/en/navbar.php" ?>
 
-<div class="slide-image banner d-flex align-items-center" style="background-image: url('/assets/images/man-child.png');">
-    <div class="hero-inner m-auto">
-        <h1 class="color-white banner-title">Lorem ipsum dolor sit amet</h1>
-        <p class="color-white banner-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-    </div>
-</div>
-<main class="py-5 container">
-    <div class="row">
-        <div class="col-lg-6 mb-3">
-            <div class="success-stories-item bg-white p-3">
-                <img class="rounded" src="/assets/images/arms.png" alt="">
-                <div class="container">
-                    <h2 class="font-size-lg my-3">Lorem ipsum dolor sit amet</h2>
-                    <p class="color-dark-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                    <a href="#" class="btn btn-lg btn-purple rounded">Read more</a>
+<main class="py-5">
+    <div class="container">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb px-0">
+                <li class="breadcrumb-item">Home</li>
+                <li class="breadcrumb-item">Library</li>
+                <li class="breadcrumb-item">Data</li>
+            </ol>
+        </nav>
+        <div class="p-4 bg-white rounded">
+            <div class="row">
+                <div class="col-md-8">
+                    <p class="color-dark-grey font-weight-bold">SUCCESS STORY — JAN 27, 2021</p>
+                    <h1 class="color-dark-purple mw-715px font-size-xxl2">Lorem ipsum dolor sit amet</h1>
+                    <div class="person d-flex">
+                        <div class="avatar-wrapper">
+                            <img src="/assets/images/man.png" alt="">
+                        </div>
+                        <div class="person-data">
+                            <p class="name color-black mb-0">Hussain AlJasmi</p>
+                        </div>
+                    </div>
+                    <div class="my-5 h-450px">
+                        <img src="/assets/images/arms.png" class="w-100 h-100 br-20" alt="">
+                    </div>
+                    <div class="font-size-md2 color-black paragraphs-container">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tincidunt pulvinar feugiat. Cras vehicula felis vel felis dapibus volutpat. Proin pharetra, massa et pharetra ultrices, dolor nunc efficitur felis, in fermentum velit odio sit amet metus. Maecenas aliquam ante sed est bibendum vestibulum. Nunc interdum eros quis mauris rhoncus, a viverra tortor consectetur. Fusce elementum felis in nisi consectetur, at rhoncus ex condimentum. Integer eget lectus eleifend, vulputate nibh a, varius orci. Curabitur non sapien sed nisi fringilla convallis. Aenean tempus mi sit amet purus bibendum, eu sodales metus ornare. Morbi vulputate, sem ac luctus placerat, velit neque tincidunt elit, eu pharetra ligula libero non urna. Nam nec odio diam. Donec odio massa, imperdiet vitae vestibulum sed, facilisis vel magna. Quisque bibendum, ante dictum blandit ornare, ex nunc tempor diam, a ultrices tortor nulla facilisis nibh. Aliquam faucibus elementum facilisis. Vestibulum consequat a sem ut vulputate.
+                        </p>
+                        <p>
+                            Interdum et malesuada fames ac ante ipsum primis in faucibus. Morbi venenatis, libero rhoncus cursus convallis, quam turpis tempus ex, sit amet mollis dui erat non ante. Sed lectus felis, faucibus id tortor at, elementum tristique sapien. Proin tempor mi ac neque pharetra convallis. Sed quis ante facilisis, pharetra nibh sit amet, volutpat quam. In dictum facilisis leo quis posuere. Integer dapibus nisi et sapien fermentum, pharetra placerat sapien posuere. Aliquam consequat condimentum consectetur. Duis vel risus nisi. Vivamus hendrerit dui vel elit interdum maximus.
+                        </p>
+                        <p>
+                            Pellentesque interdum ex non diam vestibulum porttitor. Donec placerat pellentesque nisl et tincidunt. Curabitur eu justo pellentesque, lobortis metus in, vehicula nisi. Vestibulum at faucibus orci. Phasellus eleifend nibh urna, sed porttitor turpis ultricies nec. Aenean vestibulum faucibus urna eu finibus. Vestibulum erat ante, gravida ac quam vel, finibus pharetra turpis. Aenean a leo arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed vehicula non enim aliquet imperdiet. Proin tincidunt, purus pretium sollicitudin venenatis, lacus massa consectetur lacus, non tempor eros felis non ligula. Ut ut nulla eget eros suscipit tristique. Etiam pellentesque augue eget ex commodo fringilla. Etiam dictum lacus velit, a fringilla lectus egestas in. Cras nec diam mi. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                        </p>
+                        <p>
+                            Ut maximus lacus arcu, nec pellentesque lorem bibendum luctus. Maecenas et mollis lorem. Nullam volutpat elit at lorem ullamcorper, nec auctor lacus fermentum. Sed quam nunc, tristique eu dolor non, elementum consequat velit. Nunc vel vulputate enim. Donec varius neque eget rutrum condimentum. Etiam ac ex felis. Cras accumsan scelerisque ex, id feugiat leo auctor ut. Ut auctor lorem iaculis eros porta auctor. Aenean at egestas risus. Curabitur ut rhoncus justo, id venenatis dolor. Integer auctor ac eros id interdum.
+                        </p>
+                        <p>
+                            Donec elementum, purus id fermentum dictum, quam libero lobortis sapien, feugiat ullamcorper erat arcu condimentum erat. Suspendisse sed vestibulum nisi. Duis ut felis malesuada, placerat magna eget, dictum augue. Praesent odio nibh, facilisis eu nisi eu, rhoncus iaculis enim. Praesent pretium libero augue, at rhoncus mauris ultricies quis. Nam ut sapien nec neque tristique finibus. Maecenas vehicula suscipit finibus. Donec placerat imperdiet nisi, id bibendum magna. Nunc rhoncus vel metus at venenatis. Proin at tellus tellus. In ut sapien eget justo pulvinar maximus ut sed sapien.
+                        </p>
+                    </div>
+                    <hr class="mt-5">
+                    <div class="d-flex">
+                        <button class="btn btn-primary">
+                            <i class="fa fa-heart"></i>
+                            <span>Like</span>
+                            <span>42</span>
+                        </button>
+                    </div>
                 </div>
-            </div>
-        </div>
-        <div class="col-lg-6 mb-3">
-            <div class="success-stories-item bg-white p-3">
-                <img class="rounded" src="/assets/images/arms.png" alt="">
-                <div class="container">
-                    <h2 class="font-size-lg my-3">Lorem ipsum dolor sit amet</h2>
-                    <p class="color-dark-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                    <a href="#" class="btn btn-lg btn-purple rounded">Read more</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-6 mb-3">
-            <div class="success-stories-item bg-white p-3">
-                <img class="rounded" src="/assets/images/arms.png" alt="">
-                <div class="container">
-                    <h2 class="font-size-lg my-3">Lorem ipsum dolor sit amet</h2>
-                    <p class="color-dark-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                    <a href="#" class="btn btn-lg btn-purple rounded">Read more</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-6 mb-3">
-            <div class="success-stories-item bg-white p-3">
-                <img class="rounded" src="/assets/images/arms.png" alt="">
-                <div class="container">
-                    <h2 class="font-size-lg my-3">Lorem ipsum dolor sit amet</h2>
-                    <p class="color-dark-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                    <a href="#" class="btn btn-lg btn-purple rounded">Read more</a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="text-center">
-        <button class="btn btn-lg btn-purple rounded btn-wide mt-5">Load more</button>
-    </div>
-</main>
+                <div class="col-md-4">
 
+                </div>
+            </div>
+        </div>
+    </div>
+</main>u
 <?php require_once "./hmtl-components/en/footer.php" ?>
 <?php require_once "./hmtl-components/en/login.php" ?>
 <?php require_once "./hmtl-components/en/scripts.php" ?>
