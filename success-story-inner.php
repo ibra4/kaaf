57119517caaa77c33252bdfab8681b5122e42d2a
ibra4 <ibra16@itc.hu.edu.jo>
2022-06-02 00:01:40 +0300

    Success stories

--- success-story-inner.php
+++ success-story-inner.php
@@ -1,7 +1,7 @@
 <?php require_once "./hmtl-components/en/head.php" ?>
 <?php require_once "./hmtl-components/en/navbar.php" ?>
 
-<main class="py-5">
+<main id="success-story-inner" class="py-5">
     <div class="container">
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb px-0">
@@ -10,10 +10,10 @@
                 <li class="breadcrumb-item">Data</li>
             </ol>
         </nav>
-        <div class="p-4 bg-white rounded">
+        <div class="p-5 bg-white rounded">
             <div class="row">
                 <div class="col-md-8">
-                    <p class="color-dark-grey font-weight-bold">SUCCESS STORY — JAN 27, 2021</p>
+                    <p class="color-dark-grey font-size-md2 font-weight-bold">SUCCESS STORY — JAN 27, 2021</p>
                     <h1 class="color-dark-purple mw-715px font-size-xxl2">Lorem ipsum dolor sit amet</h1>
                     <div class="person d-flex">
                         <div class="avatar-wrapper">
@@ -43,17 +43,45 @@
                             Donec elementum, purus id fermentum dictum, quam libero lobortis sapien, feugiat ullamcorper erat arcu condimentum erat. Suspendisse sed vestibulum nisi. Duis ut felis malesuada, placerat magna eget, dictum augue. Praesent odio nibh, facilisis eu nisi eu, rhoncus iaculis enim. Praesent pretium libero augue, at rhoncus mauris ultricies quis. Nam ut sapien nec neque tristique finibus. Maecenas vehicula suscipit finibus. Donec placerat imperdiet nisi, id bibendum magna. Nunc rhoncus vel metus at venenatis. Proin at tellus tellus. In ut sapien eget justo pulvinar maximus ut sed sapien.
                         </p>
                     </div>
-                    <hr class="mt-5">
-                    <div class="d-flex">
-                        <button class="btn btn-primary">
+                    <hr class="my-5">
+                    <div class="d-flex flex-wrap">
+                        <button class="btn btn-primary rounded btn-social mb-3 red">
                             <i class="fa fa-heart"></i>
                             <span>Like</span>
+                            <span>42</span>
+                        </button>
+                        <button class="btn btn-primary rounded btn-social mb-3 facebook">
+                            <i class="fa-brands fa-facebook-f"></i>
+                            <span>Share</span>
+                            <span>42</span>
+                        </button>
+                        <button class="btn btn-primary rounded btn-social mb-3 twitter">
+                            <i class="fa-brands fa-twitter"></i>
+                            <span>Tweet</span>
                             <span>42</span>
                         </button>
                     </div>
                 </div>
                 <div class="col-md-4">
-
+                    <div class="border rounded br-10 px-3 py-5">
+                        <div class="success-man-wrapper text-center">
+                            <img src="/assets/images/man.png" class="br-circle mb-4" alt="">
+                            <h3 class="font-size-md mb-2">Hussain AlJasmi</h3>
+                            <h4 class="color-dark-grey font-size-sm2 mb-3">Traveler & Photographer</h4>
+                            <h5 class="font-size-sm2 mb-4">82 articles</h5>
+                            <div class="social-media-links d-flex justify-content-center flex-wrap mt-3">
+                                <a href="#" class="mr-3">
+                                    <i class="fa-brands fa-linkedin-in"></i>
+                                </a>
+                                <a href="#" class="mr-3">
+                                    <i class="fa-brands fa-youtube"></i>
+                                </a>
+                                <a href="#" class="mr-3">
+                                    <i class="fa-brands fa-facebook-f"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
