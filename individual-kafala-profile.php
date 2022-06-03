14f66008e2f135859bb946f7003ea3173924aaf9
ibra4 <ibra16@itc.hu.edu.jo>
2022-06-03 19:32:54 +0300

    build/individual-kafala-profile.html

---
+++ individual-kafala-profile.php
@@ -0,0 +1,119 @@
+<?php require_once "./hmtl-components/en/head.php" ?>
+<?php require_once "./hmtl-components/en/navbar.php" ?>
+
+<main id="individual-kafala-profile" class="container py-5">
+    <?php require_once "./hmtl-components/en/breadcrumb.php" ?>
+    <div class="bg-white rounded p-3 mb-4">
+        <div class="row">
+            <div class="col-md-5">
+                <img class="w-100" src="/assets/images/children.png" alt="">
+            </div>
+            <div class="col-md-7 individual-kafala">
+                <div class="p-5">
+                    <h3 class="font-size-xl mb-3">Hamza Naji Jamal</h3>
+                    <h3 class="font-size-xs">
+                        <img src="/assets/icon/flags/ps.svg" alt="">
+                        <span class="font-weight-normal pl-1">Palestine</span>
+                    </h3>
+                    <p class="color-dark-grey paragraph-80">The Kafalat Project is one of the largest projects that the Charitable Works Committee of the Islah Association carries out on, as the committee provides a functional cadre with the highest level of expertise to manage the project, in which the committee has achieved great successes</p>
+                    <div class="d-flex">
+                        <div class="keys">
+                            <p class="color-dark-purple font-weight-bold">Gender</p>
+                            <p class="color-dark-purple font-weight-bold">Monthly Kafala</p>
+                            <p class="color-dark-purple font-weight-bold">Birthdate</p>
+                            <p class="color-dark-purple font-weight-bold">Siblings</p>
+                            <p class="color-dark-purple font-weight-bold">Educational Level</p>
+                        </div>
+                        <div class="values ml-4">
+                            <p class="color-dark-grey font-weight-bold">Gender</p>
+                            <p class="color-dark-grey font-weight-bold">Monthly Kafala</p>
+                            <p class="color-dark-grey font-weight-bold">Birthdate</p>
+                            <p class="color-dark-grey font-weight-bold">Siblings</p>
+                            <p class="color-dark-grey font-weight-bold">Educational Level</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="individual-kafalat-content">
+                    <div class="d-flex align-items-center latest-project-input-wrapper mb-3">
+                        <span class="color-dark-purple font-weight-bold">No. of months</span>
+                        <div class="d-flex kafala-buttons justify-content-between align-items-center">
+                            <button class="btn decrease-5">
+                                <img src="/assets/icon/minus.svg" alt="">
+                            </button>
+                            <input name="num" type="text" value="5">
+                            <button class="btn increase-5">
+                                <img src="/assets/icon/plus.svg" alt="">
+                            </button>
+                        </div>
+                    </div>
+                    <div class="extra-gift-wrapper d-flex align-items-center latest-project-input-wrapper mb-3">
+                        <div class="color-secondary extra-gift d-flex align-items-center">
+                            <span class="color-dark-purple font-weight-bold pr-1">Additional gift</span>
+                            <div class="fa-question-wrapper ml-2">
+                                <i class="fa fa-question cursor-pointer"></i>
+                                <div class="popup">Lorem ipsum dolor sit amet, consectetur adipiscing elit</div>
+                            </div>
+                        </div>
+                        <div class="gift-input-wrapper mx-3">
+                            <span class="prefix">BHD</span>
+                            <input type="text" name="" id="">
+                        </div>
+                    </div>
+                    <div class="d-flex align-items-center mb-2 d-flex d-md-none">
+                        <p class="color-dark-purple font-weight-bold mb-0">Total</p>
+                        <p class="font-weight-bold mb-0 mr-3">
+                            <span class="font-size-xs2">BHD</span>
+                            <span class="font-size-sm2">1,540</span>
+                        </p>
+                    </div>
+                    <div class="d-flex align-items-center footer my-3">
+                        <p class="font-weight-bold mb-0 mr-3 d-none d-md-block">
+                            <span class="font-size-xs2">BHD</span>
+                            <span class="font-size-sm2">1,540</span>
+                        </p>
+                        <button class="btn btn-light-purple btn-md color-dark-purple d-flex align-items-center rounded">
+                            Add
+                            <i class="fa fa-shopping-cart ml-2"></i>
+                        </button>
+                        <button class="btn btn-purple btn-md rounded ml-3">Fast Donation</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bg-white rounded p-5 individual-kafala-orphans">
+        <h2 class="font-size-md mb-4">Orphans</h2>
+        <div class="paragraphs-container">
+            <p>
+                The Messenger of God, may God’s prayers and peace be upon him, said, “I and the sponsor of an orphan will be in Paradise like these two, and he pointed to the index and middle fingers, and separated them.” narrated by Muslim
+            </p>
+            <p>
+                The number of orphans sponsored by the Charitable Activities Committee has reached more than 10,000 orphans inside and outside Bahrain (22 countries). These orphans reside with their families or in orphanages affiliated with reliable educational bodies.
+            </p>
+            <p>
+                The committee supervises orphans who are sponsored outside Bahrain in cooperation and coordination with some well-known charitable societies and institutions in the international arena, and it also conducts periodic inspection visits to these institutions with the aim of following up on orphans abroad.
+            </p>
+            <p>
+                This project was launched with the beginning of the committee’s work in 1970, and since 1991 the Charitable Activities Committee has systematically implemented the project of sponsoring orphans, which has achieved great benefits for thousands of orphans whose faces were filled with joy and prayed to their sponsors with goodness and blessing.
+            </p>
+            <p>
+                The committee has many examples of distinguished graduates at the high school or university level, and some of them became a donor .., and all of that with the blessing of the hand of charity that extended to them and nurtured them through this project.
+            </p>
+            <p>
+                Sponsoring orphans has many dimensions. In addition to the great reward of the Lord of the worlds, sponsoring orphans contributes to the reform of society when the orphan becomes a good individual and is protected from displacement and loss.
+            </p>
+            <p>
+                What happens after the sponsorship: When you become a sponsor of the orphan, you will receive a photo of him, and after that you will receive regular reports about him to see the progress of the orphan’s progress first-hand, and the committee will make every effort to meet his living needs and improve his educational, educational, social and health conditions.
+            </p>
+            <p>
+                How to sponsor an orphan: The Charitable Activities Committee of the Islah Association gives you a unique opportunity to save these orphans through the Orphan Sponsorship Project, and in order to sponsor one or more orphans, you can contact us in the Charitable Works Committee of the Islah Association at phone 17333090 or the hotline 36006166, where one of the delegates will Orphans sponsorship by providing you with all the required information about the orphans whom you want to sponsor. The value of the monthly sponsorship for an orphan, according to his country Countries in which the project will be implemented: Bahrain, Palestine, Sudan, Albania, Ukraine, Kosovo, Iraq, Mauritania, India, Yemen, Eritrea, Afghanistan, Pakistan, Somalia, Sri Lanka.
+            </p>
+        </div>
+
+    </div>
+    <div class="extra-space"></div>
+</main>
+
+<?php require_once "./hmtl-components/en/footer.php" ?>
+<?php require_once "./hmtl-components/en/login.php" ?>
+<?php require_once "./hmtl-components/en/scripts.php" ?>
