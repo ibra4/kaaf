173474e216b1a9ea5e590c75c2c3b208901fa2f2
ibra4 <ibra16@itc.hu.edu.jo>
2022-06-08 21:16:05 +0300

    build/contact.html

--- contact.php
+++ contact.php
@@ -1,42 +1,260 @@
 <?php require_once "./hmtl-components/en/head.php" ?>
 <?php require_once "./hmtl-components/en/navbar.php" ?>
 
-<main class="py-5">
+<main id="contact-page" class="py-5">
     <div class="container position-relative cart-wrapper mt-0">
-        <div class="cart-white-section bg-white rounded p-5 py-3">
-            <div class="row">
-                <div class="col-md-8 mb-3">
-                    <h1 class="font-size-md mb-4">Contact us</h1>
-                    <div class="row">
-                        <div class="col-md-8">
-                            <form action="#" method="POST">
-                                <div class="form-group">
-                                    <label for="full_name">Full name</label>
-                                    <input type="text" id="full_name" name="full_name" class="form-control">
-                                </div>
-                                <div class="form-group">
-                                    <label for="email">Email address</label>
-                                    <input type="email" id="email" name="email" class="form-control">
-                                </div>
-                                <div class="form-group">
-                                    <label for="phone_number">Phone number</label>
-                                    <div class="form-row">
-                                        <div class="col-4">
-                                            <select name="phone_number_prefix" id="phone_number_prefix" class="form-control">
-                                                <option value="+962">+962</option>
-                                            </select>
-                                        </div>
-                                        <div class="col-8">
-                                            <input type="text" id="phone_number" name="phone_number" class="form-control">
+        <div class="cart-white-section bg-white rounded px-5 py-5 mb-3 contact-wrapper">
+            <div class="col-md-11 m-auto">
+                <div class="row">
+                    <div class="col-lg-8 col-md-6 mb-3 contact-form-wrapper">
+                        <h1 class="font-size-md mb-4">Contact us</h1>
+                        <div class="row">
+                            <div class="col-lg-7">
+                                <form action="#" method="POST">
+                                    <div class="form-group">
+                                        <label for="full_name">Full name</label>
+                                        <input type="text" id="full_name" name="full_name" class="form-control">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="email">Email address</label>
+                                        <input type="email" id="email" name="email" class="form-control">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="phone_number">Phone number</label>
+                                        <div class="form-row">
+                                            <div class="col-4">
+                                                <select name="phone_number_prefix" id="phone_number_prefix" class="form-control">
+                                                    <option value="+962">+962</option>
+                                                </select>
+                                            </div>
+                                            <div class="col-8">
+                                                <input type="text" id="phone_number" name="phone_number" class="form-control">
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            </form>
+                                    <div class="form-group">
+                                        <label for="subject">Subject</label>
+                                        <input type="text" id="subject" name="subject" class="form-control">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="subject">Message</label>
+                                        <textarea id="message" name="message" class="form-control" rows="4"></textarea>
+                                    </div>
+                                    <button class="btn btn-purple rounded">
+                                        <span class="pr-2">Send</span>
+                                        <i class="fa fa-solid fa-right-long"></i>
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 mb-3">
+                        <div class="bg-light-purple br-10 p-3 contact-info-list">
+                            <h3 class="font-size-sm mb-3">Call us</h3>
+                            <div class="mb-2">
+                                <div class="color-dark-purple font-size-xs">MAIN BRANCH</div>
+                                <div class="color-dark-grey font-size-xs">Bldg 589 Rd 718 Block 207, Muharraq</div>
+                            </div>
+                            <div class="mb-2">
+                                <div class="color-dark-purple font-size-xs">P.O. BOX</div>
+                                <div class="color-dark-grey font-size-xs">23025 - Muharraq - Bahrain</div>
+                            </div>
+                            <div class="mb-2">
+                                <div class="color-dark-purple font-size-xs">PHONE</div>
+                                <div class="color-dark-grey font-size-xs">(+973) 1733 3090</div>
+                            </div>
+                            <div class="mb-2">
+                                <div class="color-dark-purple font-size-xs">FIX</div>
+                                <div class="color-dark-grey font-size-xs">(+973) 1733 3090</div>
+                            </div>
+                            <div class="mb-2">
+                                <div class="color-dark-purple font-size-xs">HOTLINE</div>
+                                <div class="color-dark-grey font-size-xs">(+973) 36006166</div>
+                            </div>
+                            <hr class="my-4">
+                            <h3 class="font-size-sm mb-3">Working hours (Main branch)</h3>
+                            <div class="mb-2">
+                                <div class="color-dark-purple font-size-xs">ADMINISTRATION</div>
+                                <div class="color-dark-grey font-size-xs">From Sunday to Thursday: 8 AM to 3:30 PM</div>
+                            </div>
+                            <div class="mb-2">
+                                <div class="color-dark-purple font-size-xs">FINANCE (RECEIVING DONATIONS)</div>
+                                <div class="color-dark-grey font-size-xs">From Saturday to Thursday</div>
+                            </div>
+                            <div class="mb-2">
+                                <div class="font-size-xs">Morning shift</div>
+                                <div class="color-dark-grey font-size-xs">8 AM to 1 PM</div>
+                            </div>
+                            <div class="mb-2">
+                                <div class="font-size-xs">Evening shift</div>
+                                <div class="color-dark-grey font-size-xs">6 PM to 9 PM (except Thursdays)</div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-4 mb-3">
+            </div>
+        </div>
+        <div class="cart-white-section bg-white rounded px-5 py-5 mb-3">
+            <div class="col-md-11 m-auto">
+                <h3 class="font-size-md mb-4">Our Branches</h3>
+                <img class="branches-img" src="/assets/images/map-branches.png" alt="" class="w-100">
+            </div>
+        </div>
+        <div class="cart-white-section bg-white rounded px-5 py-5 mb-3">
+            <div class="col-md-11 m-auto">
+                <h3 class="font-size-md mb-4">Our Bank Account Credentials</h3>
+                <div class="row justify-content-between flex-wrap bank-account-cridentials">
+                    <div class="col-auto mb-2">
+                        <div class="color-dark-purple font-size-xs">BANK NAME</div>
+                        <div class="color-dark-grey font-size-xs">BAHRAIN ISLAMIC BANK - BISB</div>
+                    </div>
+                    <div class="col-auto mb-2">
+                        <div class="color-dark-purple font-size-xs">SWIFT CODE</div>
+                        <div class="color-dark-grey font-size-xs">BISBBHBM</div>
+                    </div>
+                    <div class="col-auto mb-2">
+                        <div class="color-dark-purple font-size-xs">IBAN</div>
+                        <div class="color-dark-grey font-size-xs">BH67BIBB00100000108158</div>
+                    </div>
+                    <div class="col-auto mb-2">
+                        <div class="color-dark-purple font-size-xs">ACCOUNT NO.</div>
+                        <div class="color-dark-grey font-size-xs">100000108158</div>
+                    </div>
+                    <div class="col-auto mb-2">
+                        <div class="color-dark-purple font-size-xs">ACCOUNT NAME</div>
+                        <div class="color-dark-grey font-size-xs">ALESLAH SOCIETY</div>
+                    </div>
                 </div>
+            </div>
+        </div>
+        <div class="cart-white-section bg-white rounded px-5 py-5 mb-3">
+            <div class="col-md-11 m-auto">
+                <h3 class="font-size-md mb-4">Branches to collect and receive donations</h3>
+                <div class="row color-dark-purple font-size-xs mb-3 d-none d-md-flex">
+                    <div class="col-md-4">BRANCH</div>
+                    <div class="col-md-4">ADDRESS</div>
+                    <div class="col-md-4">PHONE</div>
+                </div>
+                <div class="row color-dark-grey">
+                    <div class="col-md-4 mb-3">
+                        <div class="color-dark-purple font-size-xs mb-2 d-block d-md-none">BRANCH</div>
+                        <h3 class="font-size-sm2 font-weight-normal">Main Branch</h3>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <div class="color-dark-purple font-size-xs mb-2 d-block d-md-none">ADDRESS</div>
+                        <h3 class="font-size-sm2 font-weight-normal">Bldg 589 Rd 718 Block 207, Muharraq</h3>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <div class="color-dark-purple font-size-xs mb-2 d-block d-md-none">PHONE</div>
+                        <h3 class="font-size-sm2 font-weight-normal">(+973) 1732 6099</h3>
+                    </div>
+                </div>
+                <hr class="d-block d-md-none mb-3">
+                <div class="row color-dark-grey">
+                    <div class="col-md-4 mb-3">
+                        <div class="color-dark-purple font-size-xs mb-2 d-block d-md-none">BRANCH</div>
+                        <h3 class="font-size-sm2 font-weight-normal">Busaiteen</h3>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <div class="color-dark-purple font-size-xs mb-2 d-block d-md-none">ADDRESS</div>
+                        <h3 class="font-size-sm2 font-weight-normal">Bldg 2530 Rd 2651 Block 226, Busaiteen</h3>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <div class="color-dark-purple font-size-xs mb-2 d-block d-md-none">PHONE</div>
+                        <h3 class="font-size-sm2 font-weight-normal">(+973) 1732 3990</h3>
+                    </div>
+                </div>
+                <hr class="d-block d-md-none mb-3">
+                <div class="row color-dark-grey">
+                    <div class="col-md-4 mb-3">
+                        <div class="color-dark-purple font-size-xs mb-2 d-block d-md-none">BRANCH</div>
+                        <h3 class="font-size-sm2 font-weight-normal">Hidd</h3>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <div class="color-dark-purple font-size-xs mb-2 d-block d-md-none">ADDRESS</div>
+                        <h3 class="font-size-sm2 font-weight-normal">Bldg 230 Hatim Alta’i Block 109, Hidd</h3>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <div class="color-dark-purple font-size-xs mb-2 d-block d-md-none">PHONE</div>
+                        <h3 class="font-size-sm2 font-weight-normal">(+973) 1767 7634</h3>
+                    </div>
+                </div>
+                <hr class="d-block d-md-none mb-3">
+                <div class="row color-dark-grey">
+                    <div class="col-md-4 mb-3">
+                        <div class="color-dark-purple font-size-xs mb-2 d-block d-md-none">BRANCH</div>
+                        <h3 class="font-size-sm2 font-weight-normal">Juffair</h3>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <div class="color-dark-purple font-size-xs mb-2 d-block d-md-none">ADDRESS</div>
+                        <h3 class="font-size-sm2 font-weight-normal">Bldg 2306 Rd 2435 Block 324</h3>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <div class="color-dark-purple font-size-xs mb-2 d-block d-md-none">PHONE</div>
+                        <h3 class="font-size-sm2 font-weight-normal">(+973) 1736 5017</h3>
+                    </div>
+                </div>
+                <hr class="d-block d-md-none mb-3">
+                <div class="row color-dark-grey">
+                    <div class="col-md-4 mb-3">
+                        <div class="color-dark-purple font-size-xs mb-2 d-block d-md-none">BRANCH</div>
+                        <h3 class="font-size-sm2 font-weight-normal">Riffa</h3>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <div class="color-dark-purple font-size-xs mb-2 d-block d-md-none">ADDRESS</div>
+                        <h3 class="font-size-sm2 font-weight-normal">Bldg 376 Rd 3505 Block 935, East Riffa</h3>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <div class="color-dark-purple font-size-xs mb-2 d-block d-md-none">PHONE</div>
+                        <h3 class="font-size-sm2 font-weight-normal">(+973) 1749 1666</h3>
+                    </div>
+                </div>
+                <hr class="d-block d-md-none mb-3">
+                <div class="row color-dark-grey">
+                    <div class="col-md-4 mb-3">
+                        <div class="color-dark-purple font-size-xs mb-2 d-block d-md-none">BRANCH</div>
+                        <h3 class="font-size-sm2 font-weight-normal">Isa Town</h3>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <div class="color-dark-purple font-size-xs mb-2 d-block d-md-none">ADDRESS</div>
+                        <h3 class="font-size-sm2 font-weight-normal">Bldg 3995 Rd 919 Block 809, Isa Town</h3>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <div class="color-dark-purple font-size-xs mb-2 d-block d-md-none">PHONE</div>
+                        <h3 class="font-size-sm2 font-weight-normal">(+973) 1768 6220</h3>
+                    </div>
+                </div>
+                <hr class="d-block d-md-none mb-3">
+                <div class="row color-dark-grey">
+                    <div class="col-md-4 mb-3">
+                        <div class="color-dark-purple font-size-xs mb-2 d-block d-md-none">BRANCH</div>
+                        <h3 class="font-size-sm2 font-weight-normal">Hamad Town</h3>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <div class="color-dark-purple font-size-xs mb-2 d-block d-md-none">ADDRESS</div>
+                        <h3 class="font-size-sm2 font-weight-normal">Bldg 1555 Rd 821 Block 1208, Hamad Town</h3>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <div class="color-dark-purple font-size-xs mb-2 d-block d-md-none">PHONE</div>
+                        <h3 class="font-size-sm2 font-weight-normal">(+973) 1742 2771</h3>
+                    </div>
+                </div>
+                <hr class="d-block d-md-none mb-3">
+                <div class="row color-dark-grey">
+                    <div class="col-md-4 mb-3">
+                        <div class="color-dark-purple font-size-xs mb-2 d-block d-md-none">BRANCH</div>
+                        <h3 class="font-size-sm2 font-weight-normal">Women’s Department</h3>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <div class="color-dark-purple font-size-xs mb-2 d-block d-md-none">ADDRESS</div>
+                        <h3 class="font-size-sm2 font-weight-normal">Bldg 587 Rd 718 Block 207, Muharraq</h3>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <div class="color-dark-purple font-size-xs mb-2 d-block d-md-none">PHONE</div>
+                        <h3 class="font-size-sm2 font-weight-normal">(+973) 1733 2157</h3>
+                    </div>
+                </div>
+                <hr class="d-block d-md-none">
             </div>
         </div>
     </div>
