0a8fe8647c5716e0e3c40ab8a5fcab87609b1cb9
ibra4 <ibra16@itc.hu.edu.jo>
2022-06-09 21:48:51 +0300

    build/about.html

---
+++ about.php
@@ -0,0 +1,91 @@
+<?php require_once "./hmtl-components/en/head.php" ?>
+<?php require_once "./hmtl-components/en/navbar.php" ?>
+
+<main class="bg-light-purple">
+    <div class="line-banner">
+        <img src="/assets/images/children.png" alt="">
+        <h1 class="font-size-xxl color-white">Who are we?</h1>
+        <div class="vertical-line"></div>
+    </div>
+    <div class="container">
+        <div class="text-center py-5">
+            <h2 class="color-dark-purple font-size-xl mt-5 mb-3">In short,</h2>
+            <p class="mw-640px font-size-lg2 text-center m-auto">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+        </div>
+    </div>
+    <div class="d-flex flex-wrap our-message">
+        <div class="">
+            <img src="/assets/images/arms.png" alt="">
+        </div>
+        <div class="">
+            <div class="d-flex flex-column justify-content-center col-lg-9 m-auto h-100">
+                <div class="mb-4">
+                    <h2 class="font-size-xl color-dark-purple mb-2">Our Message</h2>
+                    <p class="font-size-md2">We aspire to be the leading institution and the first destination locally in charitable work, with an administration that is distinguished by mastery, creativity and transparency, and with quality projects that serve people inside and outside Bahrain, focusing primarily on local community concerns, supporting the association’s mission, and enhancing the spirit of volunteer work in the community and our effective communication with all our partners.</p>
+                </div>
+                <div class="mb-3">
+                    <h2 class="font-size-xl color-dark-purple mb-2">Our Vision</h2>
+                    <p class="font-size-md2">Serving humanity by spreading goodness, alleviating suffering, and combating poverty and ignorance.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="py-5 text-center">
+        <h2 class="color-dark-purple font-size-xl mt-5 mb-3">Our Values</h2>
+        <ul class="d-flex flex-wrap col-lg-5 m-auto justify-content-center no-style">
+            <li class="our-values-item m-1" style="background-color: #63317D;">Honesty</li>
+            <li class="our-values-item m-1" style="background-color: #475AA8;">Partnership</li>
+            <li class="our-values-item m-1" style="background-color: #7A3C97;">Mastery</li>
+            <li class="our-values-item m-1" style="background-color: #0F857B;">Transparency</li>
+            <li class="our-values-item m-1" style="background-color: #481F66;">Serving humanity</li>
+            <li class="our-values-item m-1" style="background-color: #E06427;">Development</li>
+        </ul>
+    </div>
+    <div class="py-5 text-center">
+        <h2 class="color-dark-purple font-size-xl mt-5 mb-3">Our Goals</h2>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-6 p-3">
+                    <div class="d-flex flex-column justify-content-center align-items-center bg-white p-5 h-100">
+                        <img src="/assets/icon/love.svg" class="mb-3" alt="">
+                        <p class="font-size-md2">Providing humanitarian aid, and providing a decent life for the needy.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 p-3">
+                    <div class="d-flex flex-column justify-content-center align-items-center bg-white p-5 h-100">
+                        <img src="/assets/icon/arms.svg" class="mb-3" alt="">
+                        <p class="font-size-md2">Achieving the principle of social solidarity among the groups of society.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 p-3">
+                    <div class="d-flex flex-column justify-content-center align-items-center bg-white p-5 h-100">
+                        <img src="/assets/icon/finance.svg" class="mb-3" alt="">
+                        <p class="font-size-md2">Fighting poverty and establishing social justice.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 p-3">
+                    <div class="d-flex flex-column justify-content-center align-items-center bg-white p-5 h-100">
+                        <img src="/assets/icon/finance.svg" class="mb-3" alt="">
+                        <p class="font-size-md2">Fighting poverty and establishing social justice.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 p-3">
+                    <div class="d-flex flex-column justify-content-center align-items-center bg-white p-5 h-100">
+                        <img src="/assets/icon/finance.svg" class="mb-3" alt="">
+                        <p class="font-size-md2">Fighting poverty and establishing social justice.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 p-3">
+                    <div class="d-flex flex-column justify-content-center align-items-center bg-white p-5 h-100">
+                        <img src="/assets/icon/finance.svg" class="mb-3" alt="">
+                        <p class="font-size-md2">Fighting poverty and establishing social justice.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+<?php require_once "./hmtl-components/en/footer.php" ?>
+<?php require_once "./hmtl-components/en/login.php" ?>
+<?php require_once "./hmtl-components/en/scripts.php" ?>
