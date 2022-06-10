6ab7a7f1b34f42bc4402cbc4a1330cbc421283f2
ibra4 <ibra16@itc.hu.edu.jo>
2022-06-10 22:48:38 +0300

    build/executive.html

---
+++ executive.php
@@ -0,0 +1,77 @@
+<?php require_once "./hmtl-components/en/head.php" ?>
+<?php require_once "./hmtl-components/en/navbar.php" ?>
+
+<main class="bg-light-purple">
+    <div class="line-banner">
+        <img src="/assets/images/children.png" alt="">
+        <h1 class="font-size-xxl color-white">Executive Management</h1>
+        <div class="vertical-line"></div>
+    </div>
+    <div class="container">
+        <div class="row my-5">
+            <div class="col-lg-3 col-md-4 text-center m-auto pt-5 mb-3">
+                <img src="/assets/images/children.png" class="director-image mb-2" alt="">
+                <h1 class="font-size-md mb-2 mt-3">Khalid Mohammed Bucheeri</h1>
+                <p class="font-size-md2">Chairman of the Board of Trustees</p>
+            </div>
+        </div>
+        <div class="col-md-9 m-auto text-center">
+            <h2 class="color-dark-purple font-size-xl mt-5 mb-3">Charity Affairs Sector</h2>
+        </div>
+        <div class="row">
+            <div class="col-lg-3 col-md-4 text-center m-auto pt-5 mb-3">
+                <img src="/assets/images/children.png" class="director-image mb-2" alt="">
+                <h1 class="font-size-md mb-2 mt-3">Khalid Mohammed Bucheeri</h1>
+                <p class="font-size-md2">Chairman of the Board of Trustees</p>
+            </div>
+        </div>
+        <div class="row justify-content-center my-5">
+            <div class="col-lg-4 col-md-4 mb-4">
+                <div class="text-center">
+                    <img src="/assets/images/children.png" class="director-image mb-2" alt="">
+                    <h1 class="font-size-md mb-2 mt-3">Khalid Mohammed Bucheeri</h1>
+                    <p class="font-size-md2">Chairman of the Board of Trustees</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 mb-4">
+                <div class="text-center">
+                    <img src="/assets/images/children.png" class="director-image mb-2" alt="">
+                    <h1 class="font-size-md mb-2 mt-3">Khalid Mohammed Bucheeri</h1>
+                    <p class="font-size-md2">Chairman of the Board of Trustees</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 mb-4">
+                <div class="text-center">
+                    <img src="/assets/images/children.png" class="director-image mb-2" alt="">
+                    <h1 class="font-size-md mb-2 mt-3">Khalid Mohammed Bucheeri</h1>
+                    <p class="font-size-md2">Chairman of the Board of Trustees</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 mb-4">
+                <div class="text-center">
+                    <img src="/assets/images/children.png" class="director-image mb-2" alt="">
+                    <h1 class="font-size-md mb-2 mt-3">Khalid Mohammed Bucheeri</h1>
+                    <p class="font-size-md2">Chairman of the Board of Trustees</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 mb-4">
+                <div class="text-center">
+                    <img src="/assets/images/children.png" class="director-image mb-2" alt="">
+                    <h1 class="font-size-md mb-2 mt-3">Khalid Mohammed Bucheeri</h1>
+                    <p class="font-size-md2">Chairman of the Board of Trustees</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 mb-4">
+                <div class="text-center">
+                    <img src="/assets/images/children.png" class="director-image mb-2" alt="">
+                    <h1 class="font-size-md mb-2 mt-3">Khalid Mohammed Bucheeri</h1>
+                    <p class="font-size-md2">Chairman of the Board of Trustees</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+<?php require_once "./hmtl-components/en/footer.php" ?>
+<?php require_once "./hmtl-components/en/login.php" ?>
+<?php require_once "./hmtl-components/en/scripts.php" ?>
